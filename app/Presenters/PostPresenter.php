1d80bebd230cb297753d633f8cde0a77275f2d84
Lu <lilianalillyy@gmail.com>
2022-11-20 04:32:29 +0100

    fix: only allow to view posts when logged in

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -17,7 +17,18 @@ class PostPresenter extends BasePresenter
         parent::__construct();
     }
 
-    public function renderView(int $id) {
+    protected function startup()
+    {
+        if (!$this->getUser()->isLoggedIn()) {
+            $this->flashMessage("Nejste přihlášeni.", "danger");
+            $this->redirect("Auth:login");
+        }
+
+        parent::startup();
+    }
+
+    public function renderView(int $id)
+    {
         $post = $this->postRepository->findById($id);
 
         if (!$post) {
@@ -26,32 +37,24 @@ class PostPresenter extends BasePresenter
 
         $this->template->post = $post;
 
-        $ratings = $this->ratingRepository->findMany([ Rating::POST_ID_FIELD => $post->getId() ]);
+        $ratings = $this->ratingRepository->findMany([Rating::POST_ID_FIELD => $post->getId()]);
 
-        $this->template->likes = count(array_filter($ratings, fn (Rating $rating) => $rating->getKind() === Rating::KIND_LIKE));
-        $this->template->dislikes = count(array_filter($ratings, fn (Rating $rating) => $rating->getKind() === Rating::KIND_DISLIKE));
+        $this->template->likes = count(array_filter($ratings, fn(Rating $rating) => $rating->getKind() === Rating::KIND_LIKE));
+        $this->template->dislikes = count(array_filter($ratings, fn(Rating $rating) => $rating->getKind() === Rating::KIND_DISLIKE));
     }
 
-    public function handleLike(int $id) {
-        if (!$this->getUser()->isLoggedIn()) {
-            $this->flashMessage("Nejste přihlášeni.", "danger");
-            return;
-        }
-
+    public function handleLike(int $id)
+    {
         $this->ratingRepository->ratePost($id, $this->getUser()->getId(), Rating::KIND_LIKE);
 
-        $this->redirect(":view", [ "id" => $id ]);
+        $this->redirect(":view", ["id" => $id]);
     }
 
-    public function handleDislike(int $id) {
-        if (!$this->getUser()->isLoggedIn()) {
-            $this->flashMessage("Nejste přihlášeni.", "danger");
-            return;
-        }
-
+    public function handleDislike(int $id)
+    {
         $this->ratingRepository->ratePost($id, $this->getUser()->getId(), Rating::KIND_DISLIKE);
 
-        $this->redirect(":view", [ "id" => $id ]);
+        $this->redirect(":view", ["id" => $id]);
     }
 
 }
